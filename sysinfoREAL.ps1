58afd60962d7e7325e6c4630adf22d917037f479
UC-wildowac <77595220+UC-wildowac@users.noreply.github.com>
2021-01-31 21:43:36 -0500

    Add files via upload

---
+++ sysinfoREAL.ps1
@@ -0,0 +1,12 @@
+﻿#$Hello = "Hello, Powershell!"
+#Write-Host($Hello)
+function getIP{
+     (get-NetIPAddress).IPv4Address | Select-String "192"
+}
+
+Write-Host(getIP)
+$IP = getIP
+$Date = Get-Date
+$Body = "This machine's IP is $IP. User is $env:username. Hostname is $hostname. PowerShell version $Get-Host. Today's date is $Date"
+
+Send-MailMessage -To "wildowac@mail.uc.edu" -From "wildwildow@gmail.com" -Subject "IT3038c Windows SysInfo" -Body $Body -SmtpServer smtp.google.com -port 587 -UseSSL -Credential (Get-Credential)
