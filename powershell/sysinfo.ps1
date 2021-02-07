893dd4452fb1e465b1178fa5bc1753c5dffda412
UC-wildowac <wildowac@mail.uc.edu>
2021-02-07 16:31:28 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,9 @@
+﻿Function getIP{
+   (Get-NetIPAddress).ipv4address | Select-string"192"
+}
+
+Write-Host(getIP)
+$IP = getIP 
+
+Write-Host(“This machine’s IP is $IP”)
+Write-Host(“This machine’s IP is {0}” -f $IP)
