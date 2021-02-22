0e1948b3686fda82f36724b5d1f7fbcf61c9f4ee
UC-wildowac <77595220+UC-wildowac@users.noreply.github.com>
2021-02-21 20:41:09 -0500

    This is my Project 1 Script
    What this script does is give you the time with a "good evening" message and opens google chrome for you. The reason it says good evening is because I couldn't get the loop working to make it say different things throughout different times of the day, so i just went with what it would say at the time of creation. Hope you like it!
    
    To get the script to open the program, i went to quora of all places LOL. Here is the cited link https://www.quora.com/How-do-I-open-a-web-browser-using-powershell

---
+++ Evening with Time.ps1
@@ -0,0 +1,10 @@
+﻿
+(get-date).ToString('T')
+
+
+"Good evening! Here is your evening entertainment" 
+
+
+Start-Sleep -Seconds 4
+
+Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
